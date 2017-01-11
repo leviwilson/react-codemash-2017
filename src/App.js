dda0be6ff45a5cd02623127c1c80975c1333c1b7
Levi Wilson <levi@leviwilson.com>
2017-01-11 09:20:51 -0500

    move session into its own file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-class Session extends Component {
-  render() {
-    const { title, abstract } = this.props
-    return (
-      <div>
-        <div>{title}</div>
-        <div>{abstract}</div>
-      </div>
-    )
-  }
-}
-
-Session.propTypes = {
-  title: React.PropTypes.string.isRequired,
-  abstract: React.PropTypes.string.isRequired
-}
+import Session from './Session'
 
 class App extends Component {
   render() {
